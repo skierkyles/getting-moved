a0efdeb63bad617b5a0d0081e991bf0eaa6d89fb
Kyle <skierkyles@gmail.com>
2014-10-10 23:57:48 -0600

    Related TaskLoggs

--- RUBY/move/app/admin/task.rb
+++ RUBY/move/app/admin/task.rb
@@ -1,6 +1,19 @@
 ActiveAdmin.register Task do
   permit_params :title
 
+  show do |task|
+    attributes_table do
+      row :title
+      h4 "Logged Tasks"
+      ul do
+        task.logged_tasks.each do |lt|
+          li lt.notes
+        end
+      end
+    end
+    active_admin_comments
+  end
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
