40f083f59d25160ffb5da0e68c76711e0b8ff40f
Kyle <skierkyles@gmail.com>
2015-04-02 14:58:40 -0600

    Initalizer paperclip

--- RUBY/move/config/environments/development.rb
+++ RUBY/move/config/environments/development.rb
@@ -36,6 +36,9 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
+  Paperclip.options[:command_path] = "/usr/bin/"
+
+
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # config.action_controller.asset_host = 'http://localhost:3000' # Locally
--- RUBY/move/config/environments/production.rb
+++ RUBY/move/config/environments/production.rb
@@ -76,5 +76,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  Paperclip.options[:command_path] = "/usr/bin/"
+
   # config.action_controller.asset_host = 'http://tttapp.us' # Locally
 end
