ba22b34bcf2235e6c6cb1955a40fdcf109dd3d33
Kyle <skierkyles@gmail.com>
2015-04-02 14:42:23 -0600

    Configs

--- RUBY/move/config/environments/development.rb
+++ RUBY/move/config/environments/development.rb
@@ -38,5 +38,5 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  config.action_controller.asset_host = 'http://localhost:3000' # Locally
+  # config.action_controller.asset_host = 'http://localhost:3000' # Locally
 end
