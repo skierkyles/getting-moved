ae0f94814cd3c3358b31a7bdd200e8bf7e808ea0
Kyle Swanson <skierkyles@gmail.com>
2015-04-06 00:15:12 -0600

    Ionic update and styles

--- IONIC/Move/www/js/directives.js
+++ IONIC/Move/www/js/directives.js
@@ -2,7 +2,7 @@ var directives = angular.module('move.directives', []);
 
 directives.directive("fullScreenSection", function () {
   var func = function (scope, element, attributes) {
-    element[0].style.height = screen.height + "px";
+    element[0].style.height = (screen.height - 44) + "px";
   };
 
   return {
