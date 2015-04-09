5bf22c7497e6c9b8b3164e122aaab5cebfa4141a
Kyle <skierkyles@gmail.com>
2015-04-09 16:02:59 -0600

    Much betta looking

--- IONIC/Move/www/js/directives.js
+++ IONIC/Move/www/js/directives.js
@@ -13,13 +13,31 @@ directives.directive("fullScreenSection", function () {
 
 directives.directive("setRandomBackgroundImage", function () {
   var func = function (scope, element, attrs) {
-    console.log("setRandomBackgroundImage directive");
+    console.log("setRandomBackgroundImage directive", scope.activity);
+    var images = [];
+
+    for (var x = 0; x < scope.activity.logged_tasks.length; x++) {
+      var lt = scope.activity.logged_tasks[x];
+
+      for (var i of lt.images) {
+        images.push(i.image);
+      }
+    }
+
+    if (images.length > 0) {
+      var random_image = images[Math.floor(Math.random() * images.length)];
+      console.log("Random to set", random_image);
+
+      element[0].style.background = "linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url(" + random_image + ")"
+      element[0].style.backgroundSize = "cover";
+      element[0].style.backgroundPosition = "center";
+    }
   };
 
   return {
     link: func,
     scope: {
-      activity: "="
+      activity: '='
     }
   }
 });
